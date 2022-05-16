4f7e69d17d913204f540902c9bd5e2315e3d52ae
André Luiz Alves Costa <andrelac963@gmail.com>
2022-05-16 09:16:37 -0300

    Tela de dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,111 @@
 import React from 'react';
-import { View, Text } from "react-native"
+
+import { HighlightCard } from '../../Components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../Components/TransactionCard';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList
+} from './styles'
+
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: '2',
+      type: "negative",
+      title: "Hamgurgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date: "10/04/2020",
+    },
+    {
+      id: '3',
+      type: "negative",
+      title: "Aluguel",
+      amount: "R$ 1.200,00",
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag'
+      },
+      date: "10/04/2020",
+    },
+  ];
+
   return(
-    <View>
-      <Text>a</Text>
-    </View>
-    
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/84928950?v=4'}} />
+            <User>
+              <UserGreting>Olá, </UserGreting>
+              <UserName>André</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+        type='up'
+        title='Entradas'
+        amount='R$ 17.400,00'
+        lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HighlightCard
+        type='down'
+        title='Saídas'
+        amount='R$ 1.259,00'
+        lastTransaction='Última saída dia 03 de abril'
+        />
+        <HighlightCard
+        type='total'
+        title='Entradas'
+        amount='R$ 16.141,00'
+        lastTransaction='01 à 16 de abril'
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionsList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+
+      </Transactions>
+    </Container>
   )
 }
